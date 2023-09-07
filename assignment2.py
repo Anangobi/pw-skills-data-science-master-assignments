0201213dc88f17906153fb1c536992d961c817f4
GOBI ANAN R <96707488+Anangobi@users.noreply.github.com>
2023-09-07 18:03:20 +0530

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,4 +1,3 @@
 n=int(input("Enter the number"))
-ans=[x for x in range(2, n)
-     if all(x % y != 0 for y in range(2, x))]
+ans=[x for x in range(2, n) if all(x % y != 0 for y in range(2, x))]
 print(ans)
